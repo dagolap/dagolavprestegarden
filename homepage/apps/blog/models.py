a8885ecb849da3476937264274189bdf9956eda0
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-09-23 13:46:17 +0200

    Adding initial blog model and migration

--- homepage/apps/blog/models.py
+++ homepage/apps/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=False, verbose_name=_("title"))
+    ingress = models.TextField(null=True, verbose_name=_("ingress"))
+    body = models.TextField(null=True, verbose_name=_("body"))
+    author = models.ForeignKey(User ,related_name="posts", null=False, verbose_name=_("author"))
+    created = models.DateTimeField(null=False, verbose_name=_("created"))
+
+    def __unicode__(self):
+        return self.title
